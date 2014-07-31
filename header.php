cc16c53e73bc32cc2a52ad4eaaf8b8c4dfec7158
Rudy Pelletier <RudyPell782@gmail.com>
2014-07-31 14:07:56 -0400

    add header and footer

---
+++ header.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Firehouse Discount Fuels - Connecticut Home Heating</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+     <!-- Required -->
+    <link href="css/global-style.css" rel="stylesheet" type="text/css" media="screen">
+    <link rel="icon" href="images/favicon.png" type="image/png"><!-- LayerSlider stylesheet -->
+<link rel="stylesheet" href="assets/layerslider/css/layerslider.css" type="text/css">
+
+</head>
+<body class="body-bg-2">
+<div class="wrapper boxed">
+
+<div class="top-header">
+	<div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+            	<span class="aux-text hidden-xs">
+<a href="mailtto:service@firehousediscountoil.com">service@firehousediscountoil.com</a>   | (860) 485-5224 or (860) 404-1827
+                </span>
+            	<nav class="top-header-menu">
+                  
+            </div>
+        </div>
+    </div>
+</div> 
+<!-- Header: Logo and Main Nav -->
+<header>    
+	<div id="navOne" class="navbar navbar-wp" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+            	<button type="button" class="navbar-toggle navbar-toggle-aside-menu">
+                    <i class="fa fa-outdent icon-custom"></i>
+                </button>
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="index.html" title="Firehouse Discount Fuels | Great Value. No Compromise">
+                	<img src="images/boomerang-logo-dark.png" alt="Firehouse Discount Fuels | Great Value. No Compromise">
+                </a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
+                	<li class="active">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">Home</a>
+                        
+                	</li>
+                    <li class="dropdown">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">Order Online</a>
+                	</li>
+                    
+                    <li class="dropdown">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">Services</a>
+                        
+                	</li>
+                    <li class="dropdown">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">About</a>
+                        
+                	</li>
+                    <li class="dropdown">
+                    	<a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-close-others="true">Contact</a>
+                       
+                	</li>
+                    
+                </ul>
+               
+            </div><!--/.nav-collapse -->
+        </div>
+    </div>
+</header>
